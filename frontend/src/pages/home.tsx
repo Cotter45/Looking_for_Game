8f50e74e98d305dd771d321aff0f4fb39b40d519
Sean <sd.cotter45@gmail.com>
2022-05-11 06:23:15 -0400

    azure pipline test4

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { Modal } from "../context/modal/modal";
 import Splash from "../components/home";
 
