89b88f8133d747cd22a0474552f5b9c9cc88af9f
Sean <sd.cotter45@gmail.com>
2022-05-13 20:06:48 -0400

    gonna test pm2 in prod

---
+++ backend/prod.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [{
+    name: 'lfg_api',
+    script: 'bin/www',
+    node_args : '-r dotenv/config',
+    watch: '.',
+    instances: '4',
+    exec_mode: 'cluster',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
