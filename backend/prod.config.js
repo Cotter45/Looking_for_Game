d64fa2c802da9f1f652dee680a76a66f1cc16b88
Sean <sd.cotter45@gmail.com>
2022-05-13 20:18:41 -0400

    pm2 heroku test3

--- backend/prod.config.js
+++ backend/prod.config.js
@@ -4,20 +4,10 @@ module.exports = {
     script: 'bin/www',
     node_args : '-r dotenv/config',
     watch: '.',
-    instances: '4',
-    exec_mode: 'cluster'
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+    instances: 'max',
+    exec_mode: 'cluster',
+    env_production: {
+      NODE_ENV: 'production'
     }
-  }
+  }]
 };
