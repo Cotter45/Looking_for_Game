0eb44cdd469ce06c25834e95cd512c46269b80ee
Sean <sd.cotter45@gmail.com>
2022-05-21 08:26:57 -0400

    testing css keyframes for monitor splash

--- frontend/src/components/home/monitor_default.tsx
+++ frontend/src/components/home/monitor_default.tsx
@@ -1,13 +1,16 @@
 
+import background from "./mario_background.png";
+import mario from "./mario.png";
 
 function MonitorDefault() {
 
   return (
     <div className='fade_in monitor_display'>
-      <h2>Looking for Game</h2>
-      <p>
-        Is a website, desktop and mobile app for you to cut through the crap and find some people to game with. Tired of chat lobbies? Me too.
-      </p>
+      {/* <img src={background} className='scene' alt='Mario Background' /> */}
+      <div style={{ background: `url(${background}) 0 60% repeat-x` }} className="scene"></div>
+        <input type="checkbox" checked></input>
+        <img src={mario} className='mario' alt='Mario' />
+      {/* </div> */}
     </div>
   )
 }
